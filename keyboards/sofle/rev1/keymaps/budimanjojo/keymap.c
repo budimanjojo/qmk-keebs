7dc1311ace2afd7a35c01a8978278425228a26a0
budimanjojo <budimanjojo@gmail.com>
2024-04-01 12:53:17 +0700

    feat: implement bottom row mods

--- keyboards/sofle/rev1/keymaps/budimanjojo/keymap.c
+++ keyboards/sofle/rev1/keymaps/budimanjojo/keymap.c
@@ -18,7 +18,17 @@
 #include QMK_KEYBOARD_H
 #include "oled.c"
 
-//Default keymap. This can be changed in Via.
+// bottom row mods aliases
+#define CTL_Z LCTL_T(KC_Z)
+#define GUI_X LGUI_T(KC_X)
+#define ALT_C LALT_T(KC_C)
+#define SFT_V LSFT_T(KC_V)
+#define SFT_M RSFT_T(KC_M)
+#define ALT_CM LALT_T(KC_COMM)
+#define GUI_DT RGUI_T(KC_DOT)
+#define CTL_SL RCTL_T(KC_SLSH)
+
+// Default keymap. This can be changed in Via.
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*
@@ -30,7 +40,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Esc  |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
  * |------+------+------+------+------+------|  Caps |    | Pscr  |------+------+------+------+------+------|
- * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
+ * |LShift|   Z  |   X  |   C  |   V  |   B  |       |    |       |   N  |   M  |   ,  |   .  |   /  |RShift|
+ * |      | LCTL | LGUI | LALT | LSFT |      |-------|    |-------|      | RSFT | LALT | RGUI | RCTL |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *            | LCTR | LGUI | LAlt |Space | /  DEL  /       \ Bspc \  |Enter |   [  |  ]   |  \   |
  *            |      |      |      |LOWER |/       /         \      \ |RAISE |      |      |      |
@@ -41,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
     KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL,
     KC_ESC,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,  KC_CAPS, KC_PSCR, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+    KC_LSFT,  CTL_Z,   GUI_X,   ALT_C,   SFT_V,   KC_B,  KC_CAPS, KC_PSCR, KC_N,    SFT_M,   ALT_CM,  GUI_DT,  CTL_SL,  KC_RSFT,
               KC_LCTL, KC_LGUI, KC_LALT, LT(1, KC_SPC),  KC_DEL,  KC_BSPC, LT(2, KC_ENT),    KC_LBRC, KC_RBRC, KC_BSLS
 ),
 /* LOWER
@@ -127,6 +138,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         /* KEYBOARD PET STATUS START */
         case KC_LCTL:
         case KC_RCTL:
+        case CTL_Z:
+        case CTL_SL:
             if (record->event.pressed) {
                 isSneaking = true;
             } else {
@@ -145,16 +158,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             /* KEYBOARD PET STATUS END */
     }
     return true;
-}
-
-uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case LT(1, KC_SPC):
-        case LT(2, KC_ENT):
-            return 200;
-        default:
-            return TAPPING_TERM;
-    }
 }
 
 uint16_t get_quick_tap_term(uint16_t keycode, keyrecord_t *record) {
